32d098e54f7979486388102da6b30f8b59d20e7a
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-01-03 21:01:32 +0200

    succeed reading elevation raster

--- DataParsers/elavation_parser.py
+++ DataParsers/elavation_parser.py
@@ -2,8 +2,18 @@ from DataBase.DataBaseDataTypes.data_base_data_batch import DBBatch
 from DataBase.DataBaseDataTypes.data_base_range import DBRange
 from DataParsers.parser import Parser
 
+import rasterio
+import rasterio.plot
+
 
 class ElevationParser(Parser):
 
     def parse(self, data_range: DBRange) -> DBBatch:
         pass
+
+
+if __name__ == '__main__':
+    data_name = "/Users/tomerisraeli/Library/CloudStorage/GoogleDrive-tomer.israeli.43@gmail.com/My " \
+                "Drive/year_2/Magdad/data_samples/elevation/elevation final wgs/elevatinowgs.tif "
+    tiff = rasterio.open(data_name)
+    rasterio.plot.show(tiff, title="elevation data")
