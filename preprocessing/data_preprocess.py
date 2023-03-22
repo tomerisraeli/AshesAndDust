3b3d88f3f7230c66e587139e8ae91d9c6e6c3036
Tomer Israeli <tomer_israeli@outlook.co.il>
2023-03-22 23:03:40 +0200

    created data preprocess protocol

---
+++ preprocessing/data_preprocess.py
@@ -0,0 +1,24 @@
+class DataPreprocess:
+    MAIN_RAW_DATA_DIR = ""  # a directory to store all raw data at
+
+    @staticmethod
+    def fetch(self):
+        """
+        fetch the data from its source(local or remote) and save it at the raw data directory
+
+        don't do any preprocessing at this function
+        :return: None
+        """
+
+        pass
+
+    @staticmethod
+    def preprocess(self):
+        """
+        get the data from the _raw_data_directory and fit it to the coordinate system
+
+        - make sure to check that data exists
+        :return: None
+        """
+
+        pass
