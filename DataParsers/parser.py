b3136f12d4fcc8f3ccf42d84a6e1f0124baa33f6
Itay Eshkar <itayeshkar@Itays-MacBook-Air.local>
2023-04-16 09:17:31 +0300

    Adding cvs -> netCDF converter
    Adding a converter from cvs to netCDF files and vice versa

--- DataParsers/parser.py
+++ DataParsers/parser.py
@@ -1,6 +1,9 @@
 from DataBase.DataBaseDataTypes.data_base_data_batch import DBBatch
 from DataBase.DataBaseDataTypes.data_base_range import DBRange
 from support.configuration_values import ConfigurationValues
+import pandas as pd
+import xarray
+import netCDF4 as nc
 
 
 class Parser:
@@ -19,3 +22,18 @@ class Parser:
         pass
 
 
+    def cvs_to_netcdf(cvs_file_path, netcdf_file_path):
+        df = pd.read_csv(cvs_file_path)
+        xr = df.to_xarray()
+        xr.to_netcdf(netcdf_file_path)
+
+    def netcdf_to_cvs(netcdf_file_path, cvs_file_path):
+        net = nc.Dataset(netcdf_file_path, mode='r')
+        cols = list(net.variables.keys())
+        list_nc = []
+        for c in cols:
+            list_nc.append(list(net.variables[c][:]))
+        df_nc = pd.DataFrame(list_nc)
+        df_nc = df_nc.T
+        df_nc.columns = cols
+        df_nc.to_csv(cvs_file_path, index = False)
